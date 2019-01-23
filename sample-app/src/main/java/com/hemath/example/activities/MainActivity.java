0e07e2d0fb0bc789119a9f9e1388506d266ace8d
android <android@pit.com>
2019-01-23 12:18:43 +0530

    EX

---
+++ sample-app/src/main/java/com/hemath/example/activities/MainActivity.java
@@ -0,0 +1,20 @@
+package com.hemath.example.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.hemath.justifiedtext.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView justifiedParagraph = findViewById(R.id.tv_justified_paragraph);
+        justifiedParagraph.setText(R.string.lorem_ipsum_extended);
+    }
+}
